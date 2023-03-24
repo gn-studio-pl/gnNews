250ec7df4655f4a0a33a0afd96569a42ca9a4b42
DariuszMeissner <dariusz.r.meissner@gmail.com>
2023-03-24 17:42:09 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Home } from './pages'
+
+console.log(process.env.REACT_APP_NEWS_API_KEY)
+
+function App() {
+  return <Home />
+}
+
+export default App
---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+import Header from './Header'
+import Image from './Image'
+import Layout from './Layout'
+import Section from './Section'
+import Text from './Text'
+
+export { Header, Image, Layout, Section, Text }

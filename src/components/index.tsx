a071aefd4d37b302da4598fc691cc08d6457f120
DariuszMeissner <dariusz.r.meissner@gmail.com>
2023-03-26 11:52:36 +0200

    init

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,5 +3,8 @@ import Image from './Image'
 import Layout from './Layout'
 import Section from './Section'
 import Text from './Text'
+import Skeleton from './Skeleton'
+import Error from './Error'
+import Logo from './Logo'
 
-export { Header, Image, Layout, Section, Text }
+export { Header, Image, Layout, Section, Text, Skeleton, Error, Logo }

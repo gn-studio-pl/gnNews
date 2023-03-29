9adf6d77152648df57d2022fd947eb9a08c1bc45
Wolian <krzys_banasiak@o2.pl>
2023-03-29 14:45:14 +0200

    footer added

---
+++ src/features/gnNews/Footer/index.js
@@ -0,0 +1,9 @@
+import { Wrapper } from "./styled";
+
+export const Footer = () => (
+    <Wrapper>
+        <div>Aktualna godzina</div>
+        <div>Liczba artykułów: 4 </div>
+    </Wrapper>
+);
+
---
+++ src/features/gnNews/Footer/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.footer`
+    display: flex;
+    justify-content: space-between;
+    
+`;

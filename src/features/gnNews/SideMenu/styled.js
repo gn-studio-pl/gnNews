3d8dcf58ff056cc701641e6e8dff98b1695ea42e
Wolian <krzys_banasiak@o2.pl>
2023-03-29 14:46:00 +0200

    side menu added

---
+++ src/features/gnNews/SideMenu/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.aside`
+    display: flex;
+    
+`;
+
+export const CountryList = styled.ul`
+
+`;
+
+export const Item = styled.li`
+    
+`;
+
